dc6210379fae056351f1acaaf7648b71afc0b91a
steins <9182584+steins_joe@user.noreply.gitee.com>
2024-06-11 22:53:50 +0800

    feat: update utils::common module

--- src-tauri/src/utils/common.rs
+++ src-tauri/src/utils/common.rs
@@ -1,4 +1,78 @@
-//! The `common` module contains commonly used function.
+#[allow(dead_code)]
+/// Get the program name.
+/// 
+/// # Arguments
+/// 
+/// # Examples
+/// 
+/// ```
+/// use common::get_program_name;
+/// 
+/// println!("Program name: {}", get_program_name());
+/// ```
+pub fn get_program_name() -> String {
+    const NAME: Option<&str> = option_env!("CARGO_PKG_NAME");
+
+    return NAME.unwrap_or("").to_string();
+}
+
+#[allow(dead_code)]
+/// Get the program version.
+/// 
+/// # Arguments
+/// 
+/// # Examples
+/// 
+/// ```
+/// use common::get_program_version;
+/// 
+/// println!("Program version: {}", get_program_version());
+/// ```
+pub fn get_program_version() -> String {
+    const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
+
+    return VERSION.unwrap_or("").to_string();
+}
+
+#[allow(dead_code)]
+/// Get the program homepage.
+/// 
+/// If `homepage` is not defined in `cargo.toml`, it will return `""`.
+/// 
+/// # Arguments
+/// 
+/// # Examples
+/// 
+/// ```
+/// use common::get_program_homepage;
+/// 
+/// println!("Program homepage: {}", get_program_homepage());
+/// ```
+pub fn get_program_homepage() -> String {
+    const HOMEPAGE: Option<&str> = option_env!("CARGO_PKG_HOMEPAGE");
+
+    return HOMEPAGE.unwrap_or("").to_string();
+}
+
+#[allow(dead_code)]
+/// Get the program repository.
+/// 
+/// If `repository` is not defined in `cargo.toml`, it will return `""`.
+/// 
+/// # Arguments
+/// 
+/// # Examples
+/// 
+/// ```
+/// use common::get_program_repository;
+/// 
+/// println!("Program repository: {}", get_program_repository());
+/// ```
+pub fn get_program_repository() -> String {
+    const REPOSITORY: Option<&str> = option_env!("CARGO_PKG_REPOSITORY");
+
+    return REPOSITORY.unwrap_or("").to_string();
+}
 
 #[allow(dead_code)]
 /// Exit app.
@@ -106,16 +180,55 @@ pub fn get_current_timestamp() -> Result<i64, std::time::SystemTimeError> {
 /// ```
 /// use common::get_sys_locale;
 /// 
-/// let res = get_sys_locale();
-/// match res {
-///     Ok(locale) => println!("current locale: {:?}", locale),
-///     Err(error) => println!("get current locale failed, errMsg: {:?}", error),
-/// }
+/// let locale = get_sys_locale();
+/// println!("current locale: {:?}", locale),
 /// ```
-pub fn get_sys_locale() -> Result<String, std::io::Error> {
+pub fn get_sys_locale() -> String {
     use sys_locale::get_locale;
 
-    Ok(get_locale().unwrap_or_else(|| String::from("en-US")))
+    get_locale().unwrap_or_else(|| String::from("en-US"))
+}
+
+#[allow(dead_code)]
+/// Get system theme.
+/// 
+/// # Arguments
+/// 
+/// # Examples
+/// 
+/// ```
+/// use common::get_sys_theme;
+/// 
+/// let theme = get_sys_theme();
+/// println!("current theme: {:?}", theme),
+/// ```
+pub fn get_sys_theme() -> String {
+    let mode = dark_light::detect();
+
+    if mode == dark_light::Mode::Dark { "dark".to_string() } else { "light".to_string() }
+}
+
+#[allow(dead_code)]
+/// Get system webview versoin.
+/// 
+/// # Arguments
+/// 
+/// # Examples
+/// 
+/// ```
+/// use common::get_webview_version;
+/// 
+/// let res = get_webview_version();
+/// 
+/// match res {
+///     Ok(version) => println!("webview version: {:?}", version),
+///     Err(error) => println!("get webview version failed, errMsg: {:?}", error),
+/// }
+/// ```
+pub fn get_webview_version() -> Result<String, wry::Error> {
+    use wry::webview::webview_version;
+
+    webview_version()
 }
 
 #[allow(dead_code)]
@@ -151,14 +264,43 @@ pub fn open_url_with_browser(url: &str) -> Result<(), std::io::Error> {
         if let Ok(_status) = child.wait() {
             return Ok(());
         }
-        }
+    }
     
     Err(Error::from(ErrorKind::Other))
+}
+
+#[allow(dead_code)]
+/// Generate a random i32 number
+/// 
+/// # Arguments
+/// 
+/// # Examples
+/// 
+/// ```
+/// use common::rand_number;
+/// 
+/// let number = rand_number();
+/// println!("the random number is {}", number);
+/// ```
+pub fn rand_number() -> i32 {
+    use rand::Rng;
+
+    let mut rng = rand::thread_rng();
+
+    rng.gen::<i32>()
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn test_get_program_metadata() {
+        assert_eq!(get_program_name(), env!("CARGO_PKG_NAME"));
+        assert_eq!(get_program_version(), env!("CARGO_PKG_VERSION"));
+        assert_eq!(get_program_homepage(), env!("CARGO_PKG_HOMEPAGE"));
+        assert_eq!(get_program_repository(), env!("CARGO_PKG_REPOSITORY"));
+    }
 
     #[test]
     #[ignore]
@@ -191,10 +333,41 @@ mod test {
     #[test]
     fn test_get_sys_locale() {
         let sys_locale = sys_locale::get_locale().expect("en-US");
-        let locale = get_sys_locale().expect("");
+        let locale = get_sys_locale();
 
         assert_ne!(locale, "".to_string());
         assert_eq!(locale, sys_locale);
+    }
+
+    #[test]
+    fn test_get_sys_theme() {
+        let sys_theme = dark_light::detect();
+        let theme = get_sys_theme();
+
+        assert_ne!(theme, "".to_string());
+        match sys_theme {
+            dark_light::Mode::Dark => {
+                assert_eq!(theme, "dark");
+            },
+            dark_light::Mode::Light => {
+                assert_eq!(theme, "light");
+            },
+            dark_light::Mode::Default => {
+                assert_eq!(theme, "light");
+            }
+        }
+    }
+
+    #[test]
+    fn test_get_webview_version() {
+        let sys_webview_version = wry::webview::webview_version();
+        let version = get_webview_version().expect("");
+
+        if let Ok(webview_version) = sys_webview_version {
+            assert_eq!(webview_version, version);
+        } else {
+            assert_eq!(version, "");
+        }
     }
 
     #[test]
