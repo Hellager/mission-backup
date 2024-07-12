3eaef4798255ea0c55d80ce99375464a6d88165f
Hellager <steinsgu@gmail.com>
2024-07-12 23:52:01 +0800

    feat: add listen to another instance event

--- src/utils/event/index.ts
+++ src/utils/event/index.ts
@@ -1,6 +1,26 @@
 import { listen } from '@tauri-apps/api/event'
+import { ElMessage } from 'element-plus'
+import { appWindow } from '@tauri-apps/api/window'
+import i18n from '../../locales/index.ts'
 import { useSystemStore } from '../../store'
 
+/**
+ * Listens to the 'instance' event and shows a warning message.
+ */
+async function listenToInstanceEvent(): Promise<void> {
+  await listen('instance', async () => {
+    await appWindow.show()
+    await appWindow.setFocus()
+    ElMessage({
+      message: i18n.global.t('warning.anotherInstance'),
+      type: 'warning',
+    })
+  })
+}
+
+/**
+ * Listens to system theme update event
+ */
 async function listenToSysThemeEvent(): Promise<void> {
   await listen('sys_theme', (event: any) => {
     if (event.payload.code === 200) {
@@ -15,6 +35,7 @@ async function listenToSysThemeEvent(): Promise<void> {
  * Listens to multiple events by calling individual event listeners.
  */
 async function listenToEvents(): Promise<void> {
+  await listenToInstanceEvent()
   await listenToSysThemeEvent()
 }
 
